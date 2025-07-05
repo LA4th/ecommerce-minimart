e8f3abed83657ebc5d2e64efa9893f8048bc5c97
LA4th <lorenzolaforteza311@gmail.com>
2025-07-05 20:13:46 +0800

    Adding the increment

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let price = document.getElementById("prices");
 let stocks = document.getElementById("stocks");
 let cartCount = document.getElementById("count-cart");
 
-function addCartFunction() {
+let count = 0;
 
+function addCartFunction() {
+  count++;
+  cartCount.innerText = count;
 }
