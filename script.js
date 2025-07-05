552160970a7c4216b8676cfda309911836550aca
LA4th <lorenzolaforteza311@gmail.com>
2025-07-05 13:11:54 +0800

    Adding the script for function btn

---
+++ script.js
@@ -0,0 +1,7 @@
+let price = document.getElementById("prices");
+let stocks = document.getElementById("stocks");
+let cartCount = document.getElementById("count-cart");
+
+function addCartFunction() {
+
+}
