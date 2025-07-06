45c78716d12be7982c5c283ea42a78e22b00faa5
LA4th <lorenzolaforteza311@gmail.com>
2025-07-06 10:11:38 +0800

    Adding the icon and the style for card

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let price = document.getElementById("prices");
 let stocks = document.getElementById("stocks");
 let cartCount = document.getElementById("count-cart");
 
-let count = 0;
+/* let count = 0;
 
 function addCartFunction() {
   count++;
   cartCount.innerText = count;
-}
+} */
