9c377712ec96c01e6aa9aa5bf41ef7a14ec04295
LA4th <lorenzolaforteza311@gmail.com>
2025-07-07 20:19:37 +0800

    Adding the assets and figureout about the loop for objects

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-let price = document.getElementById("prices");
-let stocks = document.getElementById("stocks");
-let cartCount = document.getElementById("count-cart");
+let productItems = {"items": [
+    {
+      "foods": [
+        {
+          "name": "Rice (1kg)",
+          "price": 55,
+          "stock": 100,
+          "image": "./assets/image-bagrice.jpg"
+        },
+        {
+          "name": "Cooking Oil (1L)",
+          "price": 90,
+          "stock": 50,
+          "image": "./assets/image-oil.jpg"
+        },
+        {
+          "name": "Instant Noodles",
+          "price": 12,
+          "stock": 200,
+          "image":"./assets/image-noodles.jpg"
+        },
+        {
+          "name": "Canned Sardines",
+          "price": 25,
+          "stock": 80,
+          "image": "./assets/image-sardenes.jpg"
+        },
+        {
+          "name": "Loaf Bread",
+          "price": 50,
+          "stock": 30,
+          "image": "./assets/image-loafbread.jpg"
+        }
+      ]
+    }
+  ]
+}
 
-/* let count = 0;
+/* const productList = document.getElementById("product-list");
+const foods = productItems.items[0].foods;
 
-function addCartFunction() {
-  count++;
-  cartCount.innerText = count;
-} */
+foods.forEach(item => {
+  const card = document.createElement("div");
+  card.className = "card-container";
+  card.innerHTML = `
+    <img src="${item.image}" alt="${item.name}" />
+    <div class="card">
+      <div id="items-name">${item.name}</div>
+      <div class="stocks-container">Stocks: <span id="stocks-count">${item.stock}</span></div>
+        <div class="prices-container">Price: <span id="prices-count">$${item.price}</span></div>
+    </div>
+      <input class="addCart-btn" type="button" value="Add to Cart" onclick="addCartFunction('${item.name}')">
+  `;
+  productList.appendChild(card);
+}); */
