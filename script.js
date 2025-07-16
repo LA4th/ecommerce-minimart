33226fb3c5d99910de0fc2307014d7377bdcb842
LA4th <lorenzolaforteza311@gmail.com>
2025-07-16 08:32:15 +0800

    delete some codes

---
+++ script.js
@@ -0,0 +1,11 @@
+/* const btnCartShop = document.getElementById("btnCartShop");
+  const cartSidebar = document.getElementById("cartSidebar");
+  const closeCart = document.getElementById("closeCart");
+
+  btnCartShop.addEventListener("click", () => {
+    cartSidebar.classList.add("show");
+  });
+
+  closeCart.addEventListener("click", () => {
+    cartSidebar.classList.remove("show");
+  }); */
