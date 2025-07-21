024130b99f694fd109619017a547461a97ada767
LA4th <lorenzolaforteza311@gmail.com>
2025-07-22 07:08:23 +0800

    Adding some eventListener for cart button

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttonCart = document.querySelector("#btnCart");
+const sideBar = document.querySelector(".side-bar-container");
+const buttonClose = document.querySelector("#btnClose");
+
+let isCartPinnedOpen = false;
+
+buttonCart.addEventListener("click", () => {
+  isCartPinnedOpen = !isCartPinnedOpen;
+  if(isCartPinnedOpen) {
+    sideBar.classList.remove("hidden");
+  } else {
+    sideBar.classList.add("hidden");
+  }
+  
+});
+
+buttonClose.addEventListener("click", () => {
+  sideBar.classList.add("hidden");
+  isCartPinnedOpen = false;
+});
+
+buttonCart.addEventListener("mouseenter", () => {
+  if(!isCartPinnedOpen) {
+    sideBar.classList.remove("hidden");
+  }
+});
+
+buttonCart.addEventListener("mouseleave", () => {
+  if(!isCartPinnedOpen) {
+    sideBar.classList.add("hidden");
+  }
+});
